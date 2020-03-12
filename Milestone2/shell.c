0237437a676ba2070a198dce0a35f3e399530225
mfauzanalg <mfauzanalg@gmail.com>
2020-03-13 01:43:27 +0700

    strLength

--- Milestone2/shell.c
+++ Milestone2/shell.c
@@ -1,9 +1,49 @@
+// Primitif
+// 1. PrinsString
+// 2. PrintInteger
+// 3. PrintChar
+// 4. len (ok)
+// 5. count
+// 6. isSame (ok)
+// 7. copy
+// 8. find
+// 9. getCommandType
+// 10. concat
+// 11. replace
+// 12. split (ok)
+// 13. clear (ok)
+// 14. searchDir
+// 15. search
+
 void strLength(char* str, int *len) {
     int k;
     k = 0;
     for(k = 0; *(str+k) != 0;k++);
     (*len) = k;
 }
+
+char strCompare(char *string1, char *string2){
+    int len1, len2;
+    int i;
+    strLength(string1, &len1);
+    strLength(string2, &len2);
+
+    if (len1 == len2){
+        i = 0;
+        while (i < len1){
+            if (string1[i] != string2[i]){
+                return 0;
+            }
+            i++;
+        }
+        return 1;
+    }
+    else {
+        return 0;
+    }
+
+}
+
 
 int main (void) {
     char onShell = 1;
@@ -14,3 +54,5 @@ int main (void) {
         
     }
 }
+
+
